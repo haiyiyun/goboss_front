ccf7e7c09e406b516123c8f429e37de6e89c1427
leetaifook <lee@leetaifook.com>
2021-11-07 14:02:51 +0800

    chore: update

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,8 +1,8 @@
 // github repo url
 export const GITHUB_URL = 'https://github.com/haiyiyun/';
 
-// vue-vben-admin-next-doc
-export const DOC_URL = 'doc.haiyiyun.org/goadmin';
+// goboss_front-doc
+export const DOC_URL = 'doc.haiyiyun.org/goboss_front';
 
 // site url
 export const SITE_URL = 'https://www.haiyiyun.org';
