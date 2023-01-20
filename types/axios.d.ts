00a10e3bbdd2a6824a6a75efbae086daa0f1737b
leetaifook <lee@leetaifook.com>
2023-01-20 13:11:34 +0800

    update

--- types/axios.d.ts
+++ types/axios.d.ts
@@ -1,4 +1,5 @@
 export type ErrorMessageMode = 'none' | 'modal' | 'message' | undefined;
+export type SuccessMessageMode = ErrorMessageMode;
 
 export interface RequestOptions {
   // Splicing request parameters to url
@@ -14,15 +15,26 @@ export interface RequestOptions {
   joinPrefix?: boolean;
   // Interface address, use the default apiUrl if you leave it blank
   apiUrl?: string;
+  // 请求拼接路径
+  urlPrefix?: string;
   // Error message prompt type
   errorMessageMode?: ErrorMessageMode;
+  // Success message prompt type
+  successMessageMode?: SuccessMessageMode;
   // Whether to add a timestamp
   joinTime?: boolean;
   ignoreCancelToken?: boolean;
   // Whether to send token in header
   withToken?: boolean;
+  // 请求重试机制
+  retryRequest?: RetryRequest;
 }
 
+export interface RetryRequest {
+  isOpenRetry: boolean;
+  count: number;
+  waitTime: number;
+}
 export interface Result<T = any> {
   code: number;
   type: 'success' | 'error' | 'warning';
